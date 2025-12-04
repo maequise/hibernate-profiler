3ecac188f3d288d8022a5857a6ddc3d7a3a7d257
maequise <tahitoa.merlin@gmail.com>
2025-12-04 23:18:08 +0100

    refactor: Remove used import Remove unused declaration Apply convention variable naming

--- hibernate-profiler-spring/src/main/java/org/maequise/hibernate/profiler/configuration/ProfilerConfiguration.java
+++ hibernate-profiler-spring/src/main/java/org/maequise/hibernate/profiler/configuration/ProfilerConfiguration.java
@@ -6,24 +6,17 @@ import net.ttddyy.dsproxy.listener.logging.SystemOutQueryLoggingListener;
 import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
 import org.hibernate.engine.jdbc.internal.FormatStyle;
 import org.hibernate.engine.jdbc.internal.Formatter;
-import org.maequise.hibernate.profiler.core.DataSourceHolder;
-import org.maequise.hibernate.profiler.core.QueryInformation;
 import org.maequise.hibernate.profiler.listeners.Listener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
 import javax.sql.DataSource;
-import java.util.logging.Logger;
 
 @Configuration
-@Order(Ordered.LOWEST_PRECEDENCE)
+@Order
 public class ProfilerConfiguration {
-    private static final Logger LOGGER = Logger.getLogger(ProfilerConfiguration.class.getName());
-
     @Bean
     @Primary
     public DataSource dataSourceProxied(DataSource dataSource) {
@@ -47,11 +40,11 @@ public class ProfilerConfiguration {
 
     // use hibernate to format queries
     private static class PrettyQueryEntryCreator extends DefaultQueryLogEntryCreator {
-        private Formatter formatter = FormatStyle.BASIC.getFormatter();
+        private static final Formatter FORMATTER = FormatStyle.BASIC.getFormatter();
 
         @Override
         protected String formatQuery(String query) {
-            return this.formatter.format(query);
+            return FORMATTER.format(query);
         }
     }
 }
